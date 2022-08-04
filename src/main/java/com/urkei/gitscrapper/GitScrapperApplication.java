1d4ce657b96a61428eeb80e2c8f82478aa48a09d
Uroš Mesarič Kunst <uros.mesaric.kunst@vanderlande.com>
2022-08-04 13:39:37 +0200

    Initial commit

---
+++ src/main/java/com/urkei/gitscrapper/GitScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.urkei.gitscrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitScrapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitScrapperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/urkei/gitscrapper/GitScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urkei.gitscrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitScrapperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
